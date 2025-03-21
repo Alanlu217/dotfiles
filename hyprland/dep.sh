77afb989080ce14910bb3a4d73696cc94e0b720f
alanlu <alanlu2468@gmail.com>
2025-03-21 19:10:50 +1100

    update

--- hyprland/dep.sh
+++ hyprland/dep.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-yay -S hyprland hyprpaper rofi-wayland alacritty mako waybar pavucontrol blueman fish
+yay -S hyprland hyprpaper rofi-wayland alacritty mako\
+ waybar pavucontrol blueman fish wlogout clipse fastfetch\
+ github-cli go-task htop hyprcursor-dracula-kde-git hypridle\
+ hyprlock network-manager-applet ttf-fira-code wlogout wl-clipboard
+
