5cbb00cc9bc48f890f13486bb544b79a16eba6d1
alanlu <alanlu2468@gmail.com>
2025-05-01 18:59:47 +1000

    Add to config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,14 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/dotfiles/bin:$PATH"
 set -gx PAGER "nvim +Man!"
 set -gx MANPAGER "nvim +Man!"
 
 # Abbreviations
 alias l="eza -l --git --no-user --no-permissions"
-# alias hx="helix"
+alias vi="nvim"
 alias task="go-task"
 alias tex="tectonic -X"
 
