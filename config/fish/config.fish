f559ab358c0b2dce0b49188370c77dc0fbee35c1
alanlu <alanlu2468@gmail.com>
2025-05-01 22:07:05 +1000

    Add to config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,9 @@ if status is-interactive
 end
 
 export PATH="$HOME/dotfiles/bin:$PATH"
+
+set -gx GTK_THEME Adwaita:dark
+
 set -gx PAGER "nvim +Man!"
 set -gx MANPAGER "nvim +Man!"
 
@@ -11,6 +14,20 @@ alias l="eza -l --git --no-user --no-permissions"
 alias vi="nvim"
 alias task="go-task"
 alias tex="tectonic -X"
+
+alias gs="git status --short"
+alias gd="git diff"
+alias ga="git add"
+alias gc="git commit"
+
+alias gp="git push"
+alias gu="git pull"
+
+alias gl="git log"
+alias gb="git branch"
+
+alias gi="git init"
+alias gcl="git clone"
 
 starship init fish | source
 zoxide init fish | source
