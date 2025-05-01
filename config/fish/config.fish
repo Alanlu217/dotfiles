ab172a8cdb38837c03cad0d9a2520bc72df9a778
alanlu <alanlu2468@gmail.com>
2025-05-01 17:01:12 +1000

    Update config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,6 +4,8 @@ end
 
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/dotfiles/bin:$PATH"
+set -gx PAGER "nvim +Man!"
+set -gx MANPAGER "nvim +Man!"
 
 # Abbreviations
 alias l="eza -l --git --no-user --no-permissions"
@@ -12,3 +14,4 @@ alias task="go-task"
 alias tex="tectonic -X"
 
 starship init fish | source
+zoxide init fish | source
