97d69e90998a3bf9a0a607e1c0a549ce571992c9
alanlu <alanlu2468@gmail.com>
2025-07-31 15:19:12 +1000

    update

--- scripts/swwwrwall.sh
+++ scripts/swwwrwall.sh
@@ -3,4 +3,4 @@ cd ~/dotfiles/wallpapers
 wall="$(ls | shuf -n 1)"
 swww img --transition-step=4 --transition-fps 120 "$wall" &
 rm -rf ~/.curr_bg
-ln -s "$wall" ~/.curr_bg
+ln -s "$HOME/dotfiles/wallpapers/$wall" ~/.curr_bg
