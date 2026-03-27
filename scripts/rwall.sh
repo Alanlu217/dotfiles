7e861d6d61e6c1b5a37e894c9e8977e5cf9667ac
alanlu <alanlu2468@gmail.com>
2026-03-28 00:26:42 +1100

    update

--- scripts/rwall.sh
+++ scripts/rwall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 cd ~/dotfiles/wallpapers
 wall="$(ls | shuf -n 1)"
-swww img --transition-step=4 --transition-fps 120 "$wall" &
-swww img -n overlay --transition-step=4 --transition-fps 120 "$wall" &
+awww img --transition-step=4 --transition-fps 120 "$wall" &
+awww img -n overlay --transition-step=4 --transition-fps 120 "$wall" &
 rm -rf ~/.curr_bg
 ln -s "$HOME/dotfiles/wallpapers/$wall" ~/.curr_bg
--- scripts/swall.sh
+++ scripts/swall.sh
@@ -3,7 +3,7 @@ cd ~/dotfiles/wallpapers
 name="$(ls | rofi -dmenu)"
 if [ "${name}" == "" ]; then exit; fi
 wall="$(pwd)/../wallpapers/$name"
-swww img --transition-step=4 --transition-fps 120 "$wall" &
-swww img -n overlay --transition-step=4 --transition-fps 120 "$wall" &
+awww img --transition-step=4 --transition-fps 120 "$wall" &
+awww img -n overlay --transition-step=4 --transition-fps 120 "$wall" &
 rm -rf ~/.curr_bg
 ln -s "$wall" ~/.curr_bg
